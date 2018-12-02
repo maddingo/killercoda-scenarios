46100585147e482708ddfcee08686d406e7813bb
Martin Goldhahn <mgoldhahn@gmail.com>
2018-12-02 16:43:15 +0100

    Updated

--- wildfly-14/setupTerminal.sh
+++ wildfly-14/setupTerminal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # SDKman is run in the background, we have to wait for the file
+set +x
 while [ ! -f $HOME/.sdkman/bin/sdkman-init.sh ] ; do
     sleep 3
 done
 source $HOME/.sdkman/bin/sdkman-init.sh
+sdk default java 11.0.1-open
