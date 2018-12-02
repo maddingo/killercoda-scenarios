ade480713ec45595243659916e0c444e6625e7de
Martin Goldhahn <mgoldhahn@gmail.com>
2018-12-02 16:39:36 +0100

    Updated

--- wildfly-14/setupTerminal.sh
+++ wildfly-14/setupTerminal.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+# SDKman is run in the background, we have to wait for the file
+while [ ! -f $HOME/.sdkman/bin/sdkman-init.sh ] ; do
+    sleep 3
+done
+source $HOME/.sdkman/bin/sdkman-init.sh
