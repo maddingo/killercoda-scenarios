943ad2bc517523cdf9c68a0af95c373cb3c16696
Martin Goldhahn <martin.goldhahn@altibox.no>
2021-04-23 19:49:20 +0200

    vscode settings

--- aib-api-mgmt/02-generate-code/01_preparation.md
+++ aib-api-mgmt/02-generate-code/01_preparation.md
@@ -1,10 +1,7 @@
 To prepare for the session, clone the example repository starting in branch `02_1_attach_openapi`. This is where we left off in 
 the [Deploy Contract Track](../01-deploy-contract).
 
-`git clone -b 02_1_attach_openapi https://github.com/maddingo/api-handling.git`{{execute}}
-
 We install the contract to the local maven repository so that it is available for the next step.
 
-`cd api-handling`{{execute}}
+`cd api-handling && mvn install`{{execute}}
 
-`mvn install`{{execute}}
--- aib-api-mgmt/02-generate-code/prepare_env.sh
+++ aib-api-mgmt/02-generate-code/prepare_env.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 apt install -y libarchive-tools
+
+# install zipfs extension
 curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/arcanis/vsextensions/vscode-zipfs/2.3.0/vspackage | bsdtar -xvf - extension
 mv extension /opt/.katacodacode/extensions/zipfs-extension
 
+# Modify vscode config
+mkdir -p /opt/.katacodacode/user-data/User/
+cat > /opt/.katacodacode/user-data/User/settings.json << VSCODEEOF
+{ 
+  "workbench.startupEditor": "none", 
+  "files.autoSave": "afterDelay",
+  "editor.minimap.enabled": false,
+  "window.autoDetectColorScheme": false,
+  "workbench.colorCustomizations": {},
+  "workbench.colorTheme": "Default Dark+",
+  "editor.semanticHighlighting.enabled": false,
+  "editor.tokenColorCustomizations": {
+      "semanticHighlighting": true
+  },
+  "files.exclude": {
+    "**/.*": true
+  }
+}
+VSCODEEOF
+
+# clone playground repository
+git clone -b 02_1_attach_openapi https://github.com/maddingo/api-handling.git
