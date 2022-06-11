774efc766549e41b531664583f06ec2cfd3a6a99
Martin Goldhahn <mgoldhahn@gmail.com>
2022-06-11 23:43:24 +0200

    install prereqs

--- api-mgmt/01-deploy-contract/prepare.sh
+++ api-mgmt/01-deploy-contract/prepare.sh
@@ -4,7 +4,6 @@ MAVEN_VERSION=3.8.6
 
 apt-get update -y
 apt-get install -y openjdk-11-jdk
-
 wget https://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz -P /tmp
 tar xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
 ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
