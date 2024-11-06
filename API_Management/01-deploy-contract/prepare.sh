6a24ee29dd681bfd4ba3033eaafadaf4302a9adb
Martin Goldhahn <martin.goldhahn@lyse.no>
2024-11-06 08:01:49 +0100

    fix prepare script

--- API_Management/01-deploy-contract/prepare.sh
+++ API_Management/01-deploy-contract/prepare.sh
@@ -5,18 +5,18 @@ set -e
 
 echo "PS1='\h \W \$ '" >> ~/.bashrc
 
-sudo bash -c "echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections"
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
-sudo apt-get -qq update -y
-sudo apt-get install -y -qq openjdk-17-jdk openjdk-17-jre jq </dev/null >/dev/null
+apt-get -qq update -y
+apt-get install -y -qq openjdk-17-jdk openjdk-17-jre jq </dev/null >/dev/null
 
 # Installing Maven
 wget -q https://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz -P /tmp
 wget -q https://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512 -P /tmp
 echo " apache-maven-${MAVEN_VERSION}-bin.tar.gz" >>/tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512
 ( cd /tmp &&  sha512sum -c apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512)
-sudo tar xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
-sudo ln -s /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn
+tar xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
+ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
+ln -s /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn
 
 echo DONE >/tmp/DONE
