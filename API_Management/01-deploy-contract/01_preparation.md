0d2236ea29fbf188e322a3b3a6abea26cc8486d8
Martin Goldhahn <martin.goldhahn@lyse.no>
2024-11-06 07:45:43 +0100

    fix prepare script

--- API_Management/01-deploy-contract/01_preparation.md
+++ API_Management/01-deploy-contract/01_preparation.md
@@ -1,3 +1,5 @@
+Wait until the environment is set up. In Tab 1, a message `Welcome` should appear.
+
 To prepare for the session, clone the example repository starting in module step00.
 
 `git clone https://github.com/maddingo/api-handling.git`{{execute}}
--- API_Management/01-deploy-contract/prepare.sh
+++ API_Management/01-deploy-contract/prepare.sh
@@ -5,16 +5,16 @@ set -e
 
 echo "PS1='\h \W \$ '" >> ~/.bashrc
 
-apt-get -qq update -y
-apt-get install -y -qq openjdk-17-jdk openjdk-17-jre jq </dev/null >/dev/null
+sudo apt-get -qq update -y
+sudo apt-get install -y -qq openjdk-17-jdk openjdk-17-jre jq </dev/null >/dev/null
 
 # Installing Maven
 wget -q https://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz -P /tmp
 wget -q https://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512 -P /tmp
 echo " apache-maven-${MAVEN_VERSION}-bin.tar.gz" >>/tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512
 ( cd /tmp &&  sha512sum -c apache-maven-${MAVEN_VERSION}-bin.tar.gz.sha512)
-tar xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
-ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
-ln -s /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn
+sudo tar xf /tmp/apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
+sudo ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
+sudo ln -s /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn
 
 echo DONE >/tmp/DONE
